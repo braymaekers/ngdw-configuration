fe2716342db116d6aa8c6f3e2043b8bdafa9505b
p-jbarbosa <jbarbosa@pentaho.com>
2018-05-23 02:53:28 +0100

    Added encr utility to allow encryted password generation with the current kettle.properties.

---
+++ config-pdi-d1/encr.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+KETTLE_CLIENT_DIR=/home/malskat/Pentaho/design-tools/data-integration
+export KETTLE_HOME="/home/malskat/sandbox/HKEx/ngdw-configuration/config-pdi-d1"
+echo $KETTLE_HOME
+
+sh $KETTLE_CLIENT_DIR/encr.sh "$@"
